d80bc937ca8c549b4e00cc75d817946e82831967
Pardeep Rathore <39980537+rathore90@users.noreply.github.com>
2021-08-15 22:26:37 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: 'https://www.google.com/s2/favicons?domain=www.google.com',
+        icon: `https://www.google.com/s2/favicons?domain=www.google.com`,
       },
     },
   ],
